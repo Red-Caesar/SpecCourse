bc2463b0ed09d8c78b4e5e266370b88a96fb6664
Barbara Suslova <suslovavd2002@mail.ru>
2025-06-10 14:29:10 +0000

    remove old files

--- setup.py
+++ setup.py
@@ -8,30 +8,6 @@ from setuptools import Command, find_packages, setup
 ROOT_DIR = Path(__file__).parent
 
 
-def get_requirements() -> list[str]:
-    """Get Python package dependencies from requirements.txt."""
-    requirements_dir = ROOT_DIR / "requirements"
-
-    def _read_requirements(filename: str) -> list[str]:
-        with open(requirements_dir / filename) as f:
-            requirements = f.read().strip().split("\n")
-        resolved_requirements = []
-        for line in requirements:
-            if line.startswith("-r "):
-                resolved_requirements += _read_requirements(line.split()[1])
-            elif (
-                not line.startswith("--")
-                and not line.startswith("#")
-                and line.strip() != ""
-            ):
-                resolved_requirements.append(line)
-        return resolved_requirements
-
-    requirements = _read_requirements("dev.txt")
-
-    return requirements
-
-
 def install_nvm_and_node():
     """Install nvm and Node.js if not already installed"""
     home = Path.home()
